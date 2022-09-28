8bc032a2df89b06bb8870200b41861195b0e4c1f
Dmitry Slapygin <abashot@yandex.ru>
2022-09-28 23:46:35 +0300

    Updated

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+from measurement.views import ListCreateSensor, RetrieveUpdateSensor, \
+    ListAPIViewMeasurement
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+
+    path('sensors/', ListCreateSensor.as_view()),
+    path('sensors/<pk>/', RetrieveUpdateSensor.as_view()),
+    path('measurement/', ListAPIViewMeasurement.as_view()),
+
 ]
